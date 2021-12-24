15c47e8c1ccd7a364426798e10bcf389f99c7126
Estephanie Carmona <estephflcl@gmail.com>
2021-12-24 14:26:10 -0600

    updated grid and players

--- main.js
+++ main.js
@@ -1,2 +1,19 @@
-var msg = 'hello to world';
-console.log(msg);
+const prompt  = require("prompt-sync")({ sigint: true });
+
+
+console.log(`
+  |  | 
+--+--+--
+  |  | 
+--+--+--
+  |  |
+`)
+
+console.log("game start")
+
+console.log("player 1 is X, player 2 is O")
+
+while (true){
+    const move = prompt("player 1 turn");
+    console.log(`${move}`);
+}
