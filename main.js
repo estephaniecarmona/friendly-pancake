6d1d3b26f3f991d14183e6f71c62c43ee8e2a7e1
Estephanie Carmona <estephflcl@gmail.com>
2021-12-28 15:50:32 -0600

    grid update

--- main.js
+++ main.js
@@ -1,19 +1,49 @@
+
 const prompt  = require("prompt-sync")({ sigint: true });
+var tictactoeKeys = {
+    1: "bottom left",
+    2: "bottom middle",
+    3: "bottom right",
+    4: "middle left",
+    5: "middle middle",
+    6: "middle right",
+    7: "top left",
+    8: "top middle",
+    9: "top right"
+};
+var saveProgress = {
+
+    
+}
+
+var possibleWins = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [3, 6, 9], [1, 5, 9], [3, 5, 7]]
+
+function placeMove(move) {
+//placement of the x's and o's in the saveProgress
+//update saveProgress with 'move'
+}
 
 
-console.log(`
-  |  | 
---+--+--
-  |  | 
---+--+--
-  |  |
+function drawBoard(saveProgress){
+ console.log(`
+   |   | 
+---+---+---
+   |   | 
+---+---+---
+   |   | 
 `)
+}
+
+
 
 console.log("game start")
 
 console.log("player 1 is X, player 2 is O")
 
 while (true){
-    const move = prompt("player 1 turn");
+  drawBoard(saveProgress)
+  const playerTurn = 1;
+    const move = prompt(`player ${playerTurn} turn: `);
     console.log(`${move}`);
+    placeMove(move)
 }
