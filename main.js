b1a5228614e258c3457245e638da42167655ee87
Estephanie Carmona <estephflcl@gmail.com>
2021-12-29 12:08:41 -0600

    move update

--- main.js
+++ main.js
@@ -11,26 +11,26 @@ var tictactoeKeys = {
     8: "top middle",
     9: "top right"
 };
-var saveProgress = {
 
-    
-}
+
 
 var possibleWins = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [3, 6, 9], [1, 5, 9], [3, 5, 7]]
 
-function placeMove(move) {
+function placeMove(move, saveProgress) {
+   saveProgress[move] = "X" 
 //placement of the x's and o's in the saveProgress
 //update saveProgress with 'move'
+console.log(saveProgress)
 }
 
 
 function drawBoard(saveProgress){
  console.log(`
-   |   | 
----+---+---
-   |   | 
----+---+---
-   |   | 
+ ${saveProgress['7']} |  ${saveProgress['8']}  |  ${saveProgress['9']}
+   ---------------------------------
+${saveProgress['4']}  |  ${saveProgress['5']}  |  ${saveProgress['6']}
+   ---------------------------------
+${saveProgress['1']}  | ${saveProgress['2']}   | ${saveProgress['3']}
 `)
 }
 
@@ -40,10 +40,15 @@ console.log("game start")
 
 console.log("player 1 is X, player 2 is O")
 
+var saveProgress = {
+   
+   
+}
 while (true){
+
   drawBoard(saveProgress)
   const playerTurn = 1;
     const move = prompt(`player ${playerTurn} turn: `);
     console.log(`${move}`);
-    placeMove(move)
+    placeMove(move, saveProgress);
 }
